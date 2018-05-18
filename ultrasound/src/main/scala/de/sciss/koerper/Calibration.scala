0c48a4cc76ee5e46c6ac18d736205b51061cd5ee
Hanns Holger Rutz <contact@sciss.de>
2018-05-18 10:59:08 +0200

    ...

--- ultrasound/src/main/scala/de/sciss/koerper/Calibration.scala
+++ ultrasound/src/main/scala/de/sciss/koerper/Calibration.scala
@@ -13,18 +13,119 @@
 
 package de.sciss.koerper
 
-import de.sciss.file.file
+import de.sciss.file._
 import de.sciss.fscape.stream.Control
 import de.sciss.fscape.{Graph, graph}
-import de.sciss.koerper.DopplerTest.{Config, analyze, calcNumWin}
-import de.sciss.synth.io.AudioFile
+import de.sciss.koerper.DopplerTest.{Config, analyze, calcNumWin, calcWinStep}
+import de.sciss.numbers.Implicits._
+import de.sciss.synth.io.{AudioFile, AudioFileSpec}
 
 object Calibration {
   def main(args: Array[String]): Unit = {
-    run(Config())
+//     run(Config())
+//    testRead()
+    testApply(Config())
+  }
+
+  def formatTemplate(f: File, args: Any*): File = {
+    val name = f.name.format(args: _*)
+    f.replaceName(name)
+  }
+
+  def testApply(config: Config): Unit = {
+    import config._
+    val tempIn    = file("/data/projects/Koerper/audio_work/us-180512-approach-continuous-motu-%d.aif")
+    val fCalib    = file("/data/temp/test-calib.aif")
+    val specCalb  = AudioFile.readSpec(fCalib)
+//    require (specIn.sampleRate == sr)
+    require (specCalb.numFrames == numBands)
+    val tempOut   = file("/data/temp/test-remove-%d.png")
+
+    val g = Graph {
+      import graph._
+      for (ch <- 4 to 5) {
+        val fIn       = formatTemplate(tempIn , ch)
+        val fOut      = formatTemplate(tempOut, ch)
+        val specIn    = AudioFile.readSpec(fIn)
+        val numWin    = calcNumWin(specIn.numFrames, config)
+        val dctNum    = (numWin - (numWin%4)) >> 1
+        val fftSize   = dctNum << 1
+        println(s"[$ch] numWin = $numWin; numBands = $numBands; winStep = ${calcWinStep(config)}; fftSize = $fftSize")
+        val in        = AudioFileIn(file = fIn    , numChannels = 1) * gainIn
+        val calib     = AudioFileIn(file = fCalib , numChannels = 1)
+        val calibR    = RepeatWindow(calib, size = numBands, num = numWin)
+        val constQ    = analyze(in, config)
+        val norm      = constQ.sqrt // / dbMax.dbAmp
+        val min       = norm min calibR
+        val thresh    = norm - min
+        val rot       = RotateFlipMatrix(thresh, rows = numWin, columns = numBands, mode = RotateFlipMatrix.Rot90CCW)
+//        val dctNum    = numWin/2
+//        val dct       = DCT_II(rot, size = numWin, numCoeffs = dctNum /* numWin */)
+//        val dctNum    = (numWin + 1).nextPowerOfTwo >> 2
+        val rotT      = if (fftSize == numWin) rot else ResizeWindow(rot, size = numWin, stop = fftSize - numWin)
+        val dct0      = Real1FFT(rotT, size = fftSize, mode = 2) * 90.50967  // gain is arbitrary
+        val dct       = dct0.complex.mag
+        RunningMin(dct).last.poll(0, "min")
+        RunningMax(dct).last.poll(0, "max")
+        Length    (in)      .poll(0, "len-in")
+        Length    (constQ)  .poll(0, "len-cq")
+        Length    (thresh)  .poll(0, "len-thresh")
+        Length    (rot)     .poll(0, "len-rot")
+        Length    (dct0)    .poll(0, "len-dct0")
+        Length    (dct)     .poll(0, "len-dct")
+        val max       = dct.ampDb.linLin(dbMin, dbMax, 0.0, 1.0).clip()
+        val specOut   = ImageFile.Spec(width = dctNum, height = numBands, numChannels = 1)
+        ImageFileOut(file = fOut, spec = specOut, in = max)
+      }
+    }
+
+    val ctrl = Control()
+    ctrl.run(g)
+    import ctrl.config.executionContext
+    ctrl.status.foreach { _ =>
+      sys.exit()
+    }
+  }
+
+  def testRead(): Unit = {
+    val fIn   = file("/data/temp/test-calib.aif")
+    val afIn  = AudioFile.openRead(fIn)
+    try {
+      val buf   = afIn.buffer(afIn.numFrames.toInt)
+      afIn.read(buf)
+      println(buf(0).map(x => f"${x.ampDb * 0.5}%g").mkString("[", ", ", "]"))
+    } finally {
+      afIn.close()
+    }
   }
 
   def run(config: Config): Unit = {
+    import config._
+    val fIn       = file("/data/temp/us-180512-calib-continuous-motu.aif")
+    val specIn    = AudioFile.readSpec(fIn)
+    require (specIn.sampleRate == sr)
+    val fOut      = file("/data/temp/test-calib.aif")
+//    val numWin    = calcNumWin(specIn.numFrames, config)
+
+    val g = Graph {
+      import graph._
+      val in        = AudioFileIn(file = fIn, numChannels = 1) * gainIn
+      val constQ    = analyze(in, config)
+      val norm      = constQ.sqrt // / dbMax.dbAmp
+      val median    = SlidingWindowPercentile(norm, winSize = numBands, medianLen = 63,  frac = 0.9)
+      val max       = RunningWindowMax(median, size = numBands).takeRight(numBands)
+      AudioFileOut(fOut, AudioFileSpec(numChannels = 1, sampleRate = sr), in = max) // sr doesn't matter
+    }
+
+    val ctrl = Control()
+    ctrl.run(g)
+    import ctrl.config.executionContext
+    ctrl.status.foreach { _ =>
+      sys.exit()
+    }
+  }
+
+  def runOLD(config: Config): Unit = {
     import config._
     val fIn       = file("/data/temp/us-180512-calib-continuous-motu.aif")
     val specIn    = AudioFile.readSpec(fIn)
@@ -35,7 +136,8 @@ object Calibration {
     val g = Graph {
       import graph._
       val in        = AudioFileIn(file = fIn, numChannels = 1) * gainIn
-      val norm      = analyze(in, config)
+      val constQ    = analyze(in, config)
+      val norm      = constQ.ampDb.linLin(dbMin * 2, dbMax * 2, 0.0, 1.0).clip()
       val median    = SlidingWindowPercentile(norm, winSize = numBands, medianLen = 63,  frac = 0.9)
       val max       = RunningWindowMax(median, size = numBands)
 
@@ -51,5 +153,4 @@ object Calibration {
       sys.exit()
     }
   }
-
 }
--- ultrasound/src/main/scala/de/sciss/koerper/DopplerTest.scala
+++ ultrasound/src/main/scala/de/sciss/koerper/DopplerTest.scala
@@ -21,30 +21,38 @@ import de.sciss.synth.io.AudioFile
 
 object DopplerTest {
 
-  case class Config(dbMin: Double = -66.0, dbMax: Double = -18.0,
-                    freqMin: Double = 39500.0, freqMax: Double = 40000.0.squared / 39500.0,
-                    numBands: Int = 384, fftSize: Int = 8192, timeResMS: Double = 4.0, gainIn: Double = 40.0,
-                    sr: Double = 96000.0)
+  case class Config(dbMin     : Double  = -66.0,
+                    dbMax     : Double  = -18.0,
+                    freqMin   : Double  = 39500.0,
+                    freqMax   : Double  = 40000.0.squared / 39500.0,
+                    numBands  : Int     = 384,
+                    fftSize   : Int     = 8192,
+                    timeResMS : Double  = 4.0,
+                    gainIn    : Double  = 40.0,
+                    sr        : Double  = 96000.0
+                   )
 
   def main(args: Array[String]): Unit = {
     run(Config())
   }
 
+  // returns squared coefficients
   def analyze(in: GE, config: Config): GE = {
     import graph._
     import config._
-    val winStep = math.min(fftSize, (timeResMS / 1000 * sr + 0.5).toInt)
-    val minFreqN = freqMin / sr
-    val maxFreqN = freqMax / sr
-
-    val slid = Sliding(in, fftSize, winStep)
-    val winFun = GenWindow(size = fftSize, shape = GenWindow.Hann)
-    val windowed = slid * winFun
-    val rotWin = RotateWindow(windowed, size = fftSize, amount = fftSize / 2)
-    val fft = Real1FFT(rotWin, size = fftSize)
-    val constQ = ConstQ(fft, fftSize = fftSize, minFreqN = minFreqN, maxFreqN = maxFreqN, numBands = numBands)
-    val norm = constQ.ampDb.linLin(dbMin * 2, dbMax * 2, 0.0, 1.0).clip()
-    norm
+    val winStep   = calcWinStep(config)
+    val minFreqN  = freqMin / sr
+    val maxFreqN  = freqMax / sr
+    val slid      = Sliding(in, fftSize, winStep)
+//    Length(slid).poll(0, "len-slid")
+    val winFun    = GenWindow(size = fftSize, shape = GenWindow.Hann)
+    val windowed  = slid * winFun
+    val rotWin    = RotateWindow(windowed, size = fftSize, amount = fftSize / 2)
+//    Length(rotWin).poll(0, "len-rot-win")
+    val fft       = Real1FFT(rotWin, size = fftSize)
+//    Length(fft).poll(0, "len-fft")
+    val constQ    = ConstQ(fft, fftSize = fftSize, minFreqN = minFreqN, maxFreqN = maxFreqN, numBands = numBands)
+    constQ
   }
 
   def calcWinStep(config: Config): Int = {
@@ -55,22 +63,23 @@ object DopplerTest {
   def calcNumWin(numFrames: Long, config: Config): Int = {
     import config._
     val winStep   = calcWinStep(config)
-    val numWin    = ((numFrames - fftSize + winStep - 1) / winStep).toInt
+    val numWin    = ((numFrames /* - fftSize */ + winStep - 1) / winStep).toInt
     numWin
   }
 
   def run(config: Config): Unit = {
     import config._
-    val fIn       = file("/data/projects/Koerper/audio_work/us-180512-approach-continuous-motu-1.aif")
+    val fIn       = file("/data/projects/Koerper/audio_work/TestRec180517-cordial-neu1.aif")
     val specIn    = AudioFile.readSpec(fIn)
     require (specIn.sampleRate == sr)
-    val fOut      = file("/data/temp/us-test-sonogram.png")
+    val fOut      = file("/data/temp/us-test-cordial-sonogram.png")
     val numWin    = calcNumWin(specIn.numFrames, config)
 
     val g = Graph {
       import graph._
       val in        = AudioFileIn(file = fIn, numChannels = 1) * gainIn
-      val norm      = analyze(in, config)
+      val constQ    = analyze(in, config)
+      val norm      = constQ.ampDb.linLin(dbMin * 2, dbMax * 2, 0.0, 1.0).clip()
       val rotImg    = RotateFlipMatrix(norm, rows = numWin, columns = numBands, mode = RotateFlipMatrix.Rot90CCW)
       val specOut   = ImageFile.Spec(width = numWin, height = numBands, numChannels = 1)
       ImageFileOut(file = fOut, spec = specOut, in = rotImg)
